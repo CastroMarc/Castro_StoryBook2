23a38779e70d5c3469fd6546bdc59bc5e3dc417e
Marc Luiz Castro <111546344+CastroMarc@users.noreply.github.com>
2024-01-24 13:08:38 +0800

    Create page11.java

---
+++ page11.java
@@ -0,0 +1,100 @@
+package com.example.frogstory;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+
+public class page11 extends AppCompatActivity {
+    String frogName;
+    private static final int SWIPE_THRESHOLD = 100;
+    private float x1, x2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page11);
+
+        Intent i = getIntent();
+        frogName = i.getStringExtra("frogname");
+
+        findViewById(android.R.id.content).setOnTouchListener(this::handleSwipeEvent);
+
+        BackgroudMusicPlayer.start(this);
+
+    }
+    private void goBackToPage10() {
+        Intent i = new Intent(this, page10.class);
+        i.putExtra("frogname", frogName);
+        startActivity(i);
+        finish();
+    }
+    public void goToPage10(View view) {
+        Intent i = new Intent(this, page10.class);
+        i.putExtra("frogname", frogName);
+        startActivity(i);
+        finish();
+    }
+    private void goBackToPage12() {
+        Intent i = new Intent(this, page12.class);
+        i.putExtra("frogname", frogName);
+        startActivity(i);
+        finish();
+    }
+    public void goToPage12(View view) {
+        Intent i = new Intent(this, page12.class);
+        i.putExtra("frogname", frogName);
+        startActivity(i);
+        finish();
+    }
+
+    private boolean handleSwipeEvent(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+
+                if (Math.abs(deltaX) > SWIPE_THRESHOLD) {
+                    if (deltaX > 0) {
+                        Toast.makeText(this, "Back to Page 10", Toast.LENGTH_SHORT).show();
+                        goBackToPage10();
+                    } else {
+                        Toast.makeText(this, "Page 12", Toast.LENGTH_SHORT).show();
+                        goBackToPage12();
+                    }
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //Confirmation dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if the user clicked Yes the app will exit
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if the user clicked No it won't exit
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
